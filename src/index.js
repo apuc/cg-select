58e24dc19a74618a3ab3075adbab79eba7cc630d
MaxOvs <rock_maksimus@mail.ru>
2022-10-14 16:36:12 +0300

    Sort scss classes and index.js.

--- src/index.js
+++ src/index.js
@@ -1,74 +1,30 @@
 import { DropDown } from './cg-dropdown';
 
-// const dropdown = new DropDown({
-//   selector: '.cg-dropdown_one',
-//   placeholder: 'Выберите авто',
-//   items: ['BMW', 'Opel', 'Mersedes', 'MAN', 'max'],
+// ------------------------------Обычный селект--------------------
+const dropdown = new DropDown({
+  selector: '.cg-dropdown_one',
+  placeholder: 'Выберите авто',
+  items: ['BMW', 'Opel', 'Mersedes', 'MAN', 'max'],
+  multiselect: true,
+  multiselectTag: true,
+});
 
-//   multiselect: true,
-//   multiselectTag: true,
-// });
+// ------------------------------URL--------------------
+const dropdown3 = new DropDown({
+  selector: '.cg-dropdown_three',
+  placeholder: 'URL',
+  url: 'http://jsonplaceholder.typicode.com/users',
+  styles: {
+    head: {
+      background: 'black',
+      width: '350px',
+    },
+  },
+  multiselect: true,
+  multiselectTag: true,
+});
 
-// // dropdown.addItem('ZAZ');
-// dropdown.addItem('LADA');
-// dropdown.addItem('Kamaz 258');
-// // dropdown.addItem('BMW');
-// // const dropdown2 = new DropDown({
-// //   selector: '.cg-dropdown_two',
-// //   placeholder: 'SELECT CAR',
-// //   items: [
-// //     {
-// //       id: 'addaw21',
-// //       title: 'BMW',
-// //       value: 1,
-// //     },
-// //     {
-// //       id: '2414q',
-// //       title: 'Opel',
-// //       value: 2,
-// //     },
-// //     {
-// //       id: '24qwds',
-// //       title: 'Kamaz 258',
-// //       value: 3,
-// //     },
-// //     {
-// //       id: '28wds',
-// //       title: 'MAN',
-// //       value: 4,
-// //     },
-// //     {
-// //       id: '28qwds',
-// //       title: 'BOOT',
-// //       value: 5,
-// //     },
-// //   ],
-
-// //   multiselect: true,
-// //   event: 'mouseenter',
-// //   // multiselectTag: true,
-// // });
-
-// dropdown.disabled(false);
-
-// dropdown2.addItem('LADA');
-
-// //ToDo: paste the desired url;
-
-// const dropdown3 = new DropDown({
-//   selector: '.cg-dropdown_three',
-//   placeholder: 'URL',
-//   url: 'http://jsonplaceholder.typicode.com/users',
-//   styles: {
-//     head: {
-//       background: 'black',
-//       width: '350px',
-//     },
-//   },
-//   multiselect: true,
-//   multiselectTag: true,
-// });
-
+// --------------------------------Категории--------------------------
 const dropdown4 = new DropDown({
   selector: '.cg-dropdown_button',
   placeholder: 'Выберите регион',
@@ -102,12 +58,10 @@ const dropdown4 = new DropDown({
   multiselectTag: true,
 });
 
-// dropdown4.addItem('Харьков');
-// dropdown4.deleteItemAll();
-// dropdown4.selectIndex(5);
+//----------------управление с помощью кнопок----------------------------------
+/* const buttonOpen = document.querySelector('.button__open');
+ const buttonClose = document.querySelector('.button__close');
 
-// const buttonOpen = document.querySelector('.button__open');
-// const buttonClose = document.querySelector('.button__close');
-
-// dropdown4.buttonControl(buttonOpen, 'open');
-// dropdown4.buttonControl(buttonClose, 'close');
+ dropdown4.buttonControl(buttonOpen, 'open');
+ dropdown4.buttonControl(buttonClose, 'close'); 
+ */
